bb1b5f969d55240cfccc9aa941a15d66b30ddbb4
Yuri <yuridesideri12@gmail.com>
2022-11-03 20:07:34 -0300

    feat: post methods for setting users & tweets

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const tweets = [];
 
 const app = express();
 app.use(cors());
-//app.use(body-parser);
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello Server'));
 
-//app.post('/sign-up', (req, res) => console.log(req.body) )
+app.post('/sign-up', (req, res) => {users.push({...(req.body)}); res.send('OK')})
+app.post('/tweets', (req, res) => {
+    tweets.push({avatar : users.find(user => user.username === req.body.username).avatar, ...(req.body)});
+    res.send('OK');
+})
 
-app.get('/tweets', (req, res) => res.send(tweets.slice(10)));
+app.get('/tweets', (req, res) => res.send(tweets.slice(0,9)));
 
 app.listen(5000) 

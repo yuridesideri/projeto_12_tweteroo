10cd651e03b8a84698587e52934e40dde36df6a2
Venyus Boldrini <venyustech@gmail.com>
2022-11-10 20:53:54 -0300

    refactor: code review - fine adjustments

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const users = [];
 const tweets = [];
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello Server'));
+app.get("/", (req, res) => res.send("Hello Server"));
 
-app.post('/sign-up', (req, res) => {users.push({...(req.body)}); res.send('OK')})
-app.post('/tweets', (req, res) => {
-    tweets.push({avatar : users.find(user => user.username === req.body.username).avatar, ...(req.body)});
-    res.send('OK');
-})
+app.post("/sign-up", (req, res) => {
+  users.push({ ...req.body });
 
-app.get('/tweets', (req, res) => res.send(tweets.slice(0,9)));
+  res.send("OK");
+});
+app.post("/tweets", (req, res) => {
+  tweets.push({
+    avatar: users.find((user) => user.username === req.body.username).avatar,
+    ...req.body,
+  });
 
-app.listen(5000) 
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => res.send(tweets.slice(0, 9)));
+
+app.listen(5000);
